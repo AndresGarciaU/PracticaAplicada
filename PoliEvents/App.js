0f6113850debb547406c3b6a7833f25794f869af
LORDKINGHT <julioparra97@hotmail.com>
2020-03-14 17:46:36 -0500

    Corrigiendo Archivos

---
+++ PoliEvents/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+//Importo el componente Login
+import Login from './assets/components/Login';
+
+export default function App() {
+  return (
+    <Login/>
+  );
+}
---
+++ PoliEvents/assets/components/Login.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+
+
+class Login extends React.Component{
+    render(){
+        return(
+    <View style={styles.container}>
+        <Text style={styles.welcome}>Login</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="usuario"
+              />
+          <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          />
+    </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor:'blue'
+    },
+    welcome:{
+      padding:10,
+      fontSize:30,
+      textAlign:'center',
+      color:'white'
+    },
+    input: {
+      
+      borderColor:'black',
+      marginBottom:10,
+      padding:10,
+      width : "90%",
+      backgroundColor: 'white'
+  
+    }
+  });
+  
+export default Login;
