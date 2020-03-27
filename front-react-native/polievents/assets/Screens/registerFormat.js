c982c7200a16ae11633f96a2d05382baafb393c0
LORDKINGHT <julioparra97@hotmail.com>
2020-03-27 18:12:15 -0500

    Register Form
    Queda en un 80% el Register Form

--- front-react-native/polievents/assets/Screens/registerFormat.js
+++ front-react-native/polievents/assets/Screens/registerFormat.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import {Text, Button,StyleSheet, View} from 'react-native';
+import {Text,
+        FlatList,
+        StyleSheet,
+        View,
+        TextInput
+    } from 'react-native';
 import Login from './Login';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 export default class registerFormat extends React.Component{
@@ -8,21 +14,115 @@ export default class registerFormat extends React.Component{
     render(){
         const{navigate}=this.props.navigation;
         return(
-            <View>
-                <Text>FORMATO DE REGISTRO</Text>
-                <Button
-                    title="Volver"
-                    color="green"
-                    onPress={()=> navigate('Login')}
-                />
+            <View style={styles.container}>
+                <View style={styles.form}>
+                    <Text style={styles.header}>
+                        Registro
+                    </Text>
+                    <TextInput 
+                        style={styles.textInput}
+                        placeholder='Nombres'
+                        underlineColorAndroid={'transparent'}
+                    />
+                    <TextInput 
+                        style={styles.textInput}
+                        placeholder='Apellidos'
+                        underlineColorAndroid={'transparent'}
+                    />
+                    <TextInput 
+                        style={styles.textInput}
+                        placeholder='Correo'
+                        underlineColorAndroid={'transparent'}
+                    />
+                    <TextInput 
+                        style={styles.textInput}
+                        placeholder='Contraseña'
+                        secureTextEntry={true}
+                        underlineColorAndroid={'transparent'}
+                    />
+                    <TextInput 
+                        style={styles.textInput}
+                        placeholder='Razón'
+                        underlineColorAndroid={'transparent'}
+                    />
+
+                    <TouchableOpacity style={styles.registerButton}>
+                        <Text style={styles.textButton}>
+                            Registrarse
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            
+            /*
+    {
+        "mail": "jcperez@gmail.com",
+        "name1": "Juan",
+        "name2": "Carlos",
+        "lastName1": "Corredor",
+        "lastName2": "Perez",
+        "reason": "motivo-x",
+        "typeUser": "invitado"
+    }
+    
+    INTENTANDO HACER UNA FLATLIST
+                        /<FlatList
+                        data={[
+                            {key: 'Estudiante'},
+                            {key: 'Invitado'},
+                            {key: 'Colaborador'},
+                            {key: 'Administrativo'},
+                        ]}
+                        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+                    />
+            */ 
             
         );
     }
 }
 const styles = StyleSheet.create({
-    buttonBack:{
-        backgroundColor:'white'
+    container:{
+        flex:1,
+        justifyContent:'center',
+        backgroundColor:'#00355A',
+        paddingLeft:60,
+        paddingRight:60
+    },
+    form:{
+        alignSelf:'stretch',
+    },
+    header:{
+        fontSize:24,
+        color:'#fff',
+        paddingBottom:10,
+        marginBottom:40,
+        borderBottomColor:'#00B5DE',
+        borderBottomWidth:1,
+        alignContent:'center',
+        justifyContent:'center'
+    },
+    textInput:{
+        alignSelf:'stretch',
+        height:40,
+        marginBottom:30,
+        color:'#fff',
+        borderBottomColor:'#00B5DE',
+        borderBottomWidth:1,
+    },
+    registerButton:{
+        alignSelf:'stretch',
+        alignItems:'center',
+        padding:20,
+        backgroundColor:'#00B5DE',
+        borderBottomWidth:1
+    },
+    textButton:{
+        color:'#fff',
+        fontWeight:'bold',
+        fontSize:18
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
     }
 });
