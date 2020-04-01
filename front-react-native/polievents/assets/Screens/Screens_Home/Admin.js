164b2bda506bed123ca7bb6c5e5d51a5be171025
LORDKINGHT <julioparra97@hotmail.com>
2020-04-01 16:27:46 -0500

    Adicionando Eventos - En Progreso
    Estoy averiguando por que no me funciona el codigo, hice un post en StackOverflow

--- front-react-native/polievents/assets/Screens/Screens_Home/Admin.js
+++ front-react-native/polievents/assets/Screens/Screens_Home/Admin.js
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
-import {Text} from 'react-native';
+import {
+    Text,
+    View,
+    AsyncStorage
+} from 'react-native';
+
+import{events} from './Events';
 
 export default class Admin extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            events
+        }
+        try{
+            AsyncStorage.getItem(events).then((value)=>{
+                if(value!=null){
+                    console.log("Events esta completo")
+                }else{
+                    console.log("Events esta vacio")
+                }
+                this.setState({
+                    eventos:JSON.parse(value)
+                })
+            })
+        }catch(err){
+            console.log(err)
+        }
+    }
+    parseEventsData(){
+        if(this.state.events!=null){
+            console.log("Algo salio bien")
+            return this.state.events.map((eventos,i)=>{
+                return(
+                    <View key={i}>
+                        <Text>
+                            {eventos.title}
+                        </Text>
+                        <Text>
+                            {eventos.responsible}
+                        </Text>
+                        <Text>
+                            {eventos.description}
+                        </Text>
+                    </View>
+                )
+            })
+        }else{
+            console.log("Algo salio mal")
+        }
+
+    }
     render(){
         return(
-            <Text>
-            Aca van las opciones para que el administrador cree eventos entre otras opciones
-            </Text>
+            <View>
+                {this.parseEventsData()}
+                <Text>No salio nada</Text>                 
+            </View>
+            
         );
     }
 }
