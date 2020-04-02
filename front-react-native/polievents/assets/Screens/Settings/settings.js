6e4d7275e5adafc5602a6befca8e98f43558dcac
DavidVargas1213 <47476556+DavidVargas1213@users.noreply.github.com>
2020-04-01 19:53:08 -0500

    Update settings.js
    Insercion de botones

--- front-react-native/polievents/assets/Screens/Settings/settings.js
+++ front-react-native/polievents/assets/Screens/Settings/settings.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
-import {Text} from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button,color } from 'react-native';
+//import Nombre from './Settings/nombre';
+
 
 export default class settings extends React.Component{
     render(){
         return(
-            <Text>
-            opciones
-            </Text>
+            <View 
+            style={styles.container}>
+            <Button
+            title='Nombre de usuario' 
+            color= '#e0e0e0'
+            onPress={()=> navigate('nombre'
+            )} 
+            
+          />
+          <Button
+            large
+            title='Siguiendo' 
+            color= '#e0e0e0'
+            onPress={()=> navigate('Nombre'
+            )}
+          />
+          <Button
+            large
+            title='Cerrar sesion' 
+            color= '#e0e0e0'
+            onPress={()=> navigate('nombre'
+            )}
+          />
+          <Button
+            large
+            title='Gestionar Eventos' 
+            color= '#e0e0e0'
+            onPress={()=> navigate('Nombre'
+            )}
+          />
+          </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#e0e0e0',
+      backgroundColor:'#e0e0e0',
+      alignItems: 'flex-start'
+    },
+    nombreDeUsuario: {
+        backgroundColor: 'white',
+        alignItems: 'flex-start',
+        color : 'white'
+    },
+    textoBoton: {
+        color: 'black'
+    }
+    })
