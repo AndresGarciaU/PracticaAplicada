db39e6db12f227ec10d0829ef4cbf80489f696a1
admezalo <alexdanielmeza@gmail.com>
2020-04-01 20:14:18 -0500

    microservicio eventos

---
+++ back-apis-nodejs/microservice-events/resources/resources.js
@@ -0,0 +1,70 @@
+
+/* Example request for create event ok */
+/* Example request for create event ok */
+
+{
+    "titulo": "ROSTRO/MEMORIA",
+    "ciudad": "Bogotá",
+    "direccion": "Cra 25 #41-31",
+    "tipo": "Cultural",
+    "tema": "Cultural",
+    "contacto": "n/a",
+    "presentador": "Esneider Gamboa",
+    "anio": "2020",
+    "mes": "febrero",
+    "dia": "12",
+    "horaInicio": "7pm",
+    "duracion": "2",
+    "lugar": "C-203"
+}
+
+{
+    "titulo": "POLISPEAKERS: MITOS Y REALIDADES DEL CORONAVIRUS (COVID-19)",
+    "ciudad": "Bogotá",
+    "direccion": "Poli TV",
+    "tipo": "Académico",
+    "tema": "Académico",
+    "contacto": "n/a",
+    "presentador": "Esneider Gamboa",
+    "anio": "2020",
+    "mes": "marzo",
+    "dia": "25",
+    "horaInicio": "10:30 am",
+    "duracion": "1",
+    "lugar": "Desde tu hogar, vía Streaming"
+}
+
+{
+    "titulo": "NUESTRO POLI DIGITAL DAY CUMPLE 10 AÑOS",
+    "ciudad": "Bogotá",
+    "direccion": "57 # 3-00 Este",
+    "tipo": "Cultural",
+    "tema": "Sociedad Cultura y Creatividad",
+    "contacto": "n/a",
+    "presentador": "Nicolás Quintero",
+    "anio": "2020",
+    "mes": "marzo",
+    "dia": "20",
+    "horaInicio": "10:00 am",
+    "duracion": "1",
+    "lugar": "Bloque K - Auditorio Jaime Michelsen Uribe"
+}
+
+/* Example bad request for event */
+/* Example bad request for event */
+
+{
+    "titulo": "ROSTRO/MEMORIA",
+    "ciudad": "Bogotá",
+    "direccion": "Cra 25 #41-31",
+    "tipo": "Cultural",
+    "contacto": "n/a",
+    "presentador": "Esneider Gamboa",
+    "anio": "2020",
+    "mes": "febrero",
+    "dia": "12",
+    "horaInicio": "7pm",
+    "duracion": "1h",
+    "lugar": "C-203",
+}
+
