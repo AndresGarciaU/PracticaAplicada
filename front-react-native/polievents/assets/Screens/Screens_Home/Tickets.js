63290247020679798a1a35374a13c422dc6fece0
Andres <afgarcia2@poligran.edu.co>
2020-04-11 03:16:36 -0500

    probando registro
    registro sobre tickets ya que estaba relativamente vacio el tickets,para probar

--- front-react-native/polievents/assets/Screens/Screens_Home/Tickets.js
+++ front-react-native/polievents/assets/Screens/Screens_Home/Tickets.js
@@ -1,12 +1,77 @@
 import React, { Component } from 'react';
-import {Text} from 'react-native';
+import {Text,StyleSheet,View,Button,TextInput} from 'react-native';
 
 export default class Tickets extends React.Component{
-    render(){
-        return(
-            <Text>
-                Aca van los eventos que el Usuario se ha registrado
-            </Text>
-        );
+   constructor(){
+       super();
+       this.state={
+           user:'',
+           code:'',
+           typeUser:''
+
+       }
+   }
+   postData = async () =>{
+       
+    this.setState({text:'Clicked'})
+    fetch('https://limitless-crag-85743.herokuapp.com/api/polievents/login/createuser/standart',{
+    method:'POST',
+    body:{
+        "user": "davargas",
+        "code": "1520014222",
+        "typeUser": "estudiante"
     }
+}).then((response) => response.json())
+    .then((responseJson) => {
+        this.setState({
+            text: JSON.stringify(responseJson)
+        })
+    }
+    )}
+   
+    render(){
+        
+        return(
+            <View>
+            <TextInput 
+            style={styles.textInput}
+            placeholder='user'
+            onChangeText={(user)=>this.setState({user})}
+             underlineColorAndroid={'transparent'}
+             value={this.state.user}
+        />
+        <TextInput 
+            style={styles.textInput}
+            placeholder='code'
+            onChangeText={(code)=>this.setState({code})}
+            underlineColorAndroid={'transparent'}
+            value={this.state.code}
+        />
+        <TextInput 
+            style={styles.textInput}
+            placeholder='typeUser'
+            onChangeText={(typeUser)=>this.setState({typeUser})}
+            value={this.state.typeUser}
+            underlineColorAndroid={'transparent'}
+        />
+        
+        <Button onPress={this.postData}
+        title='postData'/>
+               <Text>
+                    {this.state.text}
+                </Text>
+        </View>
+        );
+        }
 }
+const styles = StyleSheet.create({
+    
+    textInput:{
+        alignSelf:'stretch',
+        height:40,
+        marginBottom:30,
+        color:'#fff',
+        borderBottomColor:'#00B5DE',
+        borderBottomWidth:1,
+    }
+});
